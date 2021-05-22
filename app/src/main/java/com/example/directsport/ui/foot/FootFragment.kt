47948aca21ed4544cbd0377e136aea2c7c3e3396
eliof <elio.forestier@esme.fr>
2021-05-22 16:06:49 +0200

    nouveau

--- app/src/main/java/com/example/directsport/ui/foot/FootFragment.kt
+++ app/src/main/java/com/example/directsport/ui/foot/FootFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.directsport.R
@@ -36,10 +35,6 @@ class FootFragment : Fragment(), FootOnClickeListener {
         textViewequipe.setOnClickListener {
             findNavController().navigate(R.id.action_nav_foot_to_equipeResultatFragment)
         }
-
-
-
-
 
         val recyclerfoot: RecyclerView = root.findViewById(R.id.recyclerViewfooot)
         layoutManager = LinearLayoutManager(requireContext())
